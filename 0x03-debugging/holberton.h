6b3f09b2b4474fea1e67924036c7e5619486ecab
neoxv <yaredoo2k4@gmail.com>
2021-02-09 20:45:05 +0000

    initial push for project 0x03 debugging

---
+++ 0x03-debugging/holberton.h
@@ -0,0 +1,10 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdio.h>
+
+void positive_or_negative(int i);
+
+#endif /* HOLBERTON_H */
+int largest_number(int, int, int);
+void print_remaining_days(int, int, int);
