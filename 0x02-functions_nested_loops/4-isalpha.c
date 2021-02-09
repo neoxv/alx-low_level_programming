9fd592067e1db515f8894d5b858cdbf6d5504119
neoxv <yaredoo2k4@gmail.com>
2021-02-09 10:44:37 +0000

    added fix on task 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -5,7 +5,7 @@
  */
 int _isalpha(int c)
 {
-if((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-return(1);
-return(0);
+if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+return (1);
+return (0);
 }
