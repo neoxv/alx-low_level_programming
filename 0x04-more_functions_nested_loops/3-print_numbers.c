1c97c4abb7bc9e7d31b0dc03ec1506d414aba851
neoxv <yaredoo2k4@gmail.com>
2021-02-11 20:59:44 +0000

    fix

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -6,7 +6,7 @@
 void print_numbers(void)
 {
 int i = 0;
-for (i = 0; i < 9; i++)
+for (i = 0; i <= 9; i++)
 _putchar('0' + i);
 _putchar('\n');
 }
--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,7 +6,7 @@
 void print_most_numbers(void)
 {
 int i = 0;
-for (i = 0; i < 9; i++)
+for (i = 0; i <= 9; i++)
 {
 if (i == 2 || i == 4)
 continue;
